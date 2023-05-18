480ff4874c323b2e8021f9d696fccbc19fff8775
Orest Voloshchuk <orest.v@gmail.com>
2023-05-18 12:34:55 +0300

    Moved match ownership to app level.

--- ui/src/components/app.tsx
+++ ui/src/components/app.tsx
@@ -2,46 +2,73 @@ import React, {useEffect, useState} from "react";
 import MatchSetup from "./match_setup";
 import DuelsList from "./duels_list";
 import {Match, MatchSetupRequest} from "../models";
-import {API_ROOT} from "../storage";
+import {API_ROOT, getMatchSetupFromLocalStorage, saveMatchSetupToLocalStorage} from "../storage";
 
 export default function App() {
-    const defaultMatchSetup: MatchSetupRequest = {ranges: {}}
-    const [matchSetup, setMatchSetup] = useState(defaultMatchSetup)
-    const [match, setMatch] = useState(null)
-    const [isLoading, setLoading] = useState(false)
+  const defaultMatchSetup: MatchSetupRequest = {ranges: {}}
+  const [matchSetup, setMatchSetup] = useState(defaultMatchSetup)
+  const [match, setMatch] = useState(null)
+  const [isLoading, setLoading] = useState(false)
 
-    const handleMatchSetup = function(r: MatchSetupRequest) {
-        setMatchSetup(r)
-    }
+  const handleMatchSetup = function (r: MatchSetupRequest) {
+    setMatchSetup(r)
+  }
 
-    useEffect(() => {
-        (async () => {
-            setLoading(true)
-            const response = await fetch(
-                `${API_ROOT}/duels`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    mode: "cors",
-                    body: JSON.stringify(matchSetup),
-                }
-            );
-            const receivedMatch : Match = JSON.parse(await response.text())
-            setLoading(false)
-            setMatch(receivedMatch)
+  useEffect(
+    () => {
+      const storedMatchSetup = getMatchSetupFromLocalStorage();
+      console.log("Loaded match from storage: ");
+      console.log(storedMatchSetup);
+      setMatchSetup(storedMatchSetup);
+    },
+    []
+  )
+  useEffect(
+    () => {
+      saveMatchSetupToLocalStorage(matchSetup)
+      const dataFetch = async () => {
+        const requestOptions = {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(matchSetup),
+        }
+        const data = await (
+          await fetch(`${API_ROOT}/duels`, requestOptions)
+        ).json()
+      }
+      dataFetch()
+    },
+    [matchSetup]
+  )
 
-        })()
-    }, [matchSetup])
+  useEffect(() => {
+    (async () => {
+      setLoading(true)
+      const response = await fetch(
+        `${API_ROOT}/duels`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          mode: "cors",
+          body: JSON.stringify(matchSetup),
+        }
+      );
+      const receivedMatch: Match = JSON.parse(await response.text())
+      setLoading(false)
+      setMatch(receivedMatch)
 
-    return (
-        <>
-            <div className="container-fluid my-4">
-                <MatchSetup onMatchSetup={handleMatchSetup}/>
-            </div>
-            <div className="container-fluid my-4">
-                <DuelsList match={match} isLoading={isLoading}/>
-            </div>
-        </>
-    )
+    })()
+  }, [matchSetup])
+
+  return (
+    <>
+      <div className="container-fluid my-4">
+        <MatchSetup onMatchSetup={handleMatchSetup} matchSetup={matchSetup}/>
+      </div>
+      <div className="container-fluid my-4">
+        <DuelsList match={match} isLoading={isLoading}/>
+      </div>
+    </>
+  )
 }
--- ui/src/components/match_setup.tsx
+++ ui/src/components/match_setup.tsx
@@ -1,92 +1,58 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {CLASSES, ClassSetupRequest, MatchSetupRequest, RANGES} from "../models";
 import ClassSetup from "./class_setup";
-import {API_ROOT, getMatchSetupFromLocalStorage, saveMatchSetupToLocalStorage} from "../storage";
 
 export interface MatchSetupProps {
-    onMatchSetup: (r: MatchSetupRequest) => void
+  matchSetup: MatchSetupRequest
+  onMatchSetup: (r: MatchSetupRequest) => void
 }
 
 export default function MatchSetup(props: MatchSetupProps) {
-    const defaultMatchSetup: MatchSetupRequest = {
-        ranges: {
-            "1": {classes: {}},
-            "2": {classes: {}},
+  const participantsChangedHandler = function (range: string, clazz: string, c: ClassSetupRequest) {
+    const newMatchSetup = {
+      ranges: {
+        ...props.matchSetup.ranges,
+        [range]: {
+          classes: {
+            ...props.matchSetup.ranges[range].classes,
+            [clazz]: c
+          }
         }
+      }
     }
+    console.log("Participant list changed:")
+    console.log(newMatchSetup)
+    props.onMatchSetup(newMatchSetup)
+  }
 
-    const [matchSetup, setMatchSetup] = useState(defaultMatchSetup)
-
-    const participantsChangedHandler = function(range: string, clazz: string, c: ClassSetupRequest) {
-        setMatchSetup(
-            {
-                ranges: {
-                    ...matchSetup.ranges,
-                    [range]: {
-                        classes: {
-                            ...matchSetup.ranges[range].classes,
-                            [clazz]: c
-                        }
-                    }
-                }
-            }
-        )
+  const ranges = []
+  for (const range of RANGES) {
+    const r = props.matchSetup.ranges[range];
+    const defaultSetup: ClassSetupRequest = {participants: [], twice: false}
+    const classes = r ? r.classes : {}
+    const classControls = []
+    for (const clazz of CLASSES) {
+      const setup = classes[clazz] || defaultSetup
+      classControls.push(
+        <div className="col" key={`${range}-${clazz}`}>
+          <ClassSetup
+            className={clazz}
+            classSetup={setup}
+            onParticipantsChanged={(c) => participantsChangedHandler(range, clazz, c)}/>
+        </div>
+      )
     }
-
-    useEffect(
-        () => {
-            const storedMatchSetup = getMatchSetupFromLocalStorage();
-            setMatchSetup(storedMatchSetup);
-        },
-        []
+    ranges.push(
+      <div className="row py-3" key={range}>
+        <h1>Рубіж {range}</h1>
+        {classControls}
+      </div>
     )
-    useEffect(
-        () => {
-            saveMatchSetupToLocalStorage(matchSetup)
-            props.onMatchSetup(matchSetup)
-            const dataFetch = async () => {
-                const requestOptions = {
-                    method: "POST",
-                    headers: {"Content-Type": "application/json"},
-                    body: JSON.stringify(matchSetup),
-                }
-                const data = await (
-                    await fetch(`${API_ROOT}/duels`, requestOptions)
-                ).json()
-            }
-            dataFetch()
-        },
-        [matchSetup]
-    )
+  }
 
-    const ranges = []
-    for (const range of RANGES) {
-        const r = matchSetup.ranges[range];
-        const defaultSetup: ClassSetupRequest = {participants: [], twice: false}
-        const classes = r ? r.classes : {}
-        const classControls = []
-        for (const clazz of CLASSES) {
-            const setup = classes[clazz] || defaultSetup
-            classControls.push(
-                <div className="col" key={`${range}-${clazz}`}>
-                    <ClassSetup
-                        className={clazz}
-                        classSetup={setup}
-                        onParticipantsChanged={(c) => participantsChangedHandler(range, clazz, c)}/>
-                </div>
-            )
-        }
-        ranges.push(
-            <div className="row py-3" key={range}>
-                <h1>Рубіж {range}</h1>
-                { classControls }
-            </div>
-        )
-    }
-
-    return (
-        <>
-            { ranges }
-        </>
-    )
+  return (
+    <>
+      {ranges}
+    </>
+  )
 }
