2f5477ce3f6837cb8ff3388dc52ee2fbe7d4ca4f
Orest Voloshchuk <orest.v@gmail.com>
2023-05-18 14:26:34 +0300

    WIP match loader.

--- ui/src/components/app.tsx
+++ ui/src/components/app.tsx
@@ -1,44 +1,37 @@
 import React, {useEffect, useState} from "react";
+import MatchLoader from "./loader";
 import MatchSetup from "./match_setup";
 import DuelsList from "./duels_list";
-import {Match, MatchSetupRequest} from "../models";
+import {EmptyMatchSetupRequest, Match, MatchSetupRequest, RangeSetupRequest} from "../models";
 import {API_ROOT, getMatchSetupFromLocalStorage, saveMatchSetupToLocalStorage} from "../storage";
 
 export default function App() {
-  const defaultMatchSetup: MatchSetupRequest = {ranges: {}}
-  const [matchSetup, setMatchSetup] = useState(defaultMatchSetup)
+  const [matchSetup, setMatchSetup] = useState(EmptyMatchSetupRequest())
   const [match, setMatch] = useState(null)
   const [isLoading, setLoading] = useState(false)
+  const [isMatchLoaderVisible, setMatchLoaderVisible] = useState(true)
 
   const handleMatchSetup = function (r: MatchSetupRequest) {
+    setMatchSetup(r)
+  }
+
+  const handleResetMatchClick = function () {
+    if (!confirm("Це скине поточне налаштування матчу: впевнені?")) {
+      return;
+    }
+    setMatchSetup(EmptyMatchSetupRequest())
+  }
+
+  const handleMatchLoaded = function(r:  MatchSetupRequest) {
     setMatchSetup(r)
   }
 
   useEffect(
     () => {
       const storedMatchSetup = getMatchSetupFromLocalStorage();
-      console.log("Loaded match from storage: ");
-      console.log(storedMatchSetup);
       setMatchSetup(storedMatchSetup);
     },
     []
-  )
-  useEffect(
-    () => {
-      saveMatchSetupToLocalStorage(matchSetup)
-      const dataFetch = async () => {
-        const requestOptions = {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(matchSetup),
-        }
-        const data = await (
-          await fetch(`${API_ROOT}/duels`, requestOptions)
-        ).json()
-      }
-      dataFetch()
-    },
-    [matchSetup]
   )
 
   useEffect(() => {
@@ -59,16 +52,33 @@ export default function App() {
       setMatch(receivedMatch)
 
     })()
+    saveMatchSetupToLocalStorage(matchSetup);
   }, [matchSetup])
 
   return (
     <>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid d-flex">
+          <h1>Дуелі</h1>
+          <button type="button" className="btn btn-primary"
+                  onClick={() => {
+                    setMatchLoaderVisible(true)
+                  }}>
+            Додати список з Practicarms
+          </button>
+          <button type={"button"} className={"btn btn-outline-danger"} onClick={handleResetMatchClick}>Очистити</button>
+        </div>
+      </nav>
+      <div className="container-fluid my-4">
+        <MatchLoader visible={isMatchLoaderVisible} onMatchLoaded={handleMatchLoaded}/>
+      </div>
       <div className="container-fluid my-4">
         <MatchSetup onMatchSetup={handleMatchSetup} matchSetup={matchSetup}/>
       </div>
       <div className="container-fluid my-4">
         <DuelsList match={match} isLoading={isLoading}/>
       </div>
+
     </>
   )
 }
--- ui/src/models.ts
+++ ui/src/models.ts
@@ -1,29 +1,54 @@
 export interface MatchSetupRequest {
-    ranges: { [key: string]: RangeSetupRequest };
+  ranges: { [key: string]: RangeSetupRequest };
 }
 
 export interface RangeSetupRequest {
-    classes: {  [key: string] : ClassSetupRequest };
+  classes: { [key: string]: ClassSetupRequest };
 }
 
 export interface ClassSetupRequest {
-    participants: string[];
-    twice:        boolean;
+  participants: string[];
+  twice: boolean;
 }
 
 export interface Match {
-    ranges: { [key: string] : Duel[]}
+  ranges: { [key: string]: Duel[] }
 }
 
 export interface Duel {
-    left: Participant
-    right: Participant
-    clazz: string
+  left: Participant
+  right: Participant
+  clazz: string
 }
 
 export interface Participant {
-    name: string
+  name: string
 }
 
 export const CLASSES = ["S", "SL", "SM", "M", "O"];
 export const RANGES = ["1", "2"];
+
+export function EmptyMatchSetupRequest(): MatchSetupRequest {
+  return {
+    ranges: {
+      "1": {
+        classes: {
+          S: {participants: [], twice: false},
+          SL: {participants: [], twice: false},
+          SM: {participants: [], twice: false},
+          M: {participants: [], twice: false},
+          O: {participants: [], twice: false},
+        }
+      },
+      "2": {
+        classes: {
+          S: {participants: [], twice: false},
+          SL: {participants: [], twice: false},
+          SM: {participants: [], twice: false},
+          M: {participants: [], twice: false},
+          O: {participants: [], twice: false},
+        }
+      }
+    }
+  }
+}
